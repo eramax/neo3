c11dc82f1b0be9198ff0f6dc5e87369ce60ee378
Ahmed Morsi <eng.eramax@gmail.com>
2025-06-03 12:48:35 +0200

    43

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
             output: {
                 manualChunks: {
                     'vendor': ['lit', 'lit-element'],
-                    'ollama': ['ollama/browser']
                 }
             }
         }
